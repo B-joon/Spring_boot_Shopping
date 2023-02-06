6ff80bb619106e466f2cbcf8458e48a1bafcbb71
B-joon <dosel45@gmail.com>
2023-02-06 22:15:02 +0900

    154 회원 가입 기능 구현하기

--- src/main/java/com/example/shopping_mall/controller/MemberContorller.java
+++ src/main/java/com/example/shopping_mall/controller/MemberContorller.java
@@ -1,11 +1,14 @@
 package com.example.shopping_mall.controller;
 
+import com.example.shopping_mall.entity.MemberEntity;
 import com.example.shopping_mall.service.MemberService;
 import com.example.shopping_mall.vo.MemberVO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,11 +17,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberContorller {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping(value = "/new")
     public String memberForm(Model model) {
         model.addAttribute("memberFormVo", new MemberVO());
         return "member/memberForm";
+    }
+
+    @PostMapping(value = "/new")
+    public String memeberForm(MemberVO membervo) {
+        MemberEntity member = MemberEntity.createMember(membervo, passwordEncoder);
+        memberService.save(member);
+        return "redirect:/";
     }
 
 }
