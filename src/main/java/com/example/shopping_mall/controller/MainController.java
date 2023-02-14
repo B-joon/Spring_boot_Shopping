8e21cc5e2b11875cf1b0e3345bb0ba3a0a7f017c
B-joon <dosel45@gmail.com>
2023-02-14 16:42:29 +0900

    main page 로직 변경

--- src/main/java/com/example/shopping_mall/controller/MainController.java
+++ src/main/java/com/example/shopping_mall/controller/MainController.java
@@ -1,13 +1,34 @@
 package com.example.shopping_mall.controller;
 
+import com.example.shopping_mall.service.ItemService;
+import com.example.shopping_mall.vo.ItemSearchVO;
+import com.example.shopping_mall.vo.MainItemVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
+    private final ItemService itemService;
+
     @GetMapping(value = "/")
-    public String main() {
+    public String main(ItemSearchVO itemSearchVO, Optional<Integer> page, Model model) {
+
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 6);
+        Page<MainItemVO> items = itemService.getMainItemPage(itemSearchVO, pageable);
+
+        model.addAttribute("items", items);
+        model.addAttribute("itemSearchVO", itemSearchVO);
+        model.addAttribute("maxPage", 5);
+
         return "main";
     }
 
