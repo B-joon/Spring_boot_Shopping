ea0bc68a1779b248afa7a140ce4316e0a9e74d22
B-joon <dosel45@gmail.com>
2023-02-14 16:44:45 +0900

    Entity 공통 변수

--- src/main/java/com/example/shopping_mall/entity/BaseEntity.java
+++ src/main/java/com/example/shopping_mall/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.example.shopping_mall.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,11 +13,12 @@ import javax.persistence.MappedSuperclass;
 @EntityListeners(value = {AuditingEntityListener.class})
 @MappedSuperclass
 @Getter
+@NoArgsConstructor
 public abstract class BaseEntity extends BaseTimeEntity{
 
     @CreatedBy
     @Column(updatable = false)
-    private String createBy;
+    private String createdBy;
 
     @LastModifiedBy
     private String modifiedBy;
