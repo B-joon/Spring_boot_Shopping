30a0bc80726ee247e86184897133befb19c84faa
B-joon <dosel45@gmail.com>
2023-02-15 16:52:17 +0900

    파일 저장, 수정, 삭제 관련 설명 추가

--- src/main/java/com/example/shopping_mall/service/FileServiceImpl.java
+++ src/main/java/com/example/shopping_mall/service/FileServiceImpl.java
@@ -14,12 +14,21 @@ public class FileServiceImpl implements FileService{
 
     @Override
     public String uploadFile(String uploadPath, String originalFileName, byte[] fileData) throws Exception {
+        /*
+        UUID(Universally Unique Identifier)는 서로 다른 객체들을 구별하기 위해서 이름을 부여할 때 사용
+        실제 사용 시 중복될 가능성이 거의 없기 때문에 파일의 이름으로 사용하면 파일명 중복 문제를 해결할 수 있다
+         */
         UUID uuid = UUID.randomUUID();
 
         String extersion = originalFileName.substring(originalFileName.lastIndexOf("."));
+        // UUID로 받은 값과 원래 파일의 이름의 확장자를 조합해서 저장될 파일 이름을 만든다.
         String saveFileName = uuid.toString() + extersion;
         String fileUploadFullUrl = uploadPath + "/" + saveFileName;
 
+        /*
+        FileOutputStream 클래스는 바이트 단위 출력을 내보내는 클래스이다.
+        생성자로 파일이 저장될 위치와 파일 이름을 넘겨 파일에 쓸 파일 출력 스트림을 만든다.
+         */
         FileOutputStream fos = new FileOutputStream(fileUploadFullUrl);
         fos.write(fileData);
         fos.close();
