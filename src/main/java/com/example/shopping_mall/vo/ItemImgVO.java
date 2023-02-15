13ff4879cb430107113206d384f668b9f325efce
B-joon <dosel45@gmail.com>
2023-02-15 16:55:06 +0900

    데이터 조회 수정 저장 관련하여 Entity와 VO를 같이 호출하는 번거로운 작업을 하지 않도록 만드는 메소드 설명 추가

--- src/main/java/com/example/shopping_mall/vo/ItemImgVO.java
+++ src/main/java/com/example/shopping_mall/vo/ItemImgVO.java
@@ -15,6 +15,9 @@ public class ItemImgVO {
     private String repImgYn;
     private static ModelMapper modelMapper = new ModelMapper();
 
+    // ItemImgEntity 객체를 파라미터로 받아 ItemImgEntity 객체의 자료형과 맴버변수의
+    // 이름이 같을 때 ItemImgVO로 값을 복사해서 반환한다.
+    // static 메소드로 선언해 ItemImgVO 객체를 생성하지 않아도 호출할 수 있도록 한다.
     public static ItemImgVO of(ItemImgEntity itemImg) {
         return modelMapper.map(itemImg, ItemImgVO.class);
     }
