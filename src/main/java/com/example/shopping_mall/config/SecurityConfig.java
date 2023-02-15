252a2bee00707127a3ff6113f74cebe232db25c1
B-joon <dosel45@gmail.com>
2023-02-15 13:14:08 +0900

    Spring Security 설명 추가

--- src/main/java/com/example/shopping_mall/config/SecurityConfig.java
+++ src/main/java/com/example/shopping_mall/config/SecurityConfig.java
@@ -34,16 +34,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/");             // 로그아웃 성공 시 이동할 URL 설정
 
         http.authorizeRequests()
+                // premitAll()을 통해 모든 사용자가 인증없이 해당 경로에 접근할 수 있도록 설정
                 .mvcMatchers("/", "/members/**", "/item/**", "images/**").permitAll()
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
+                // 위에서 설정해준 경로를 제외한 나머지 경로들은 모두 인증을 요구하도록 한다.
                 .anyRequest().authenticated();
 
         http.exceptionHandling()
+                // 인증되지 않은 사용자가 리소스에 접근하였을 때 수행되는 핸들러를 등록
                 .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
+        // 아래 경로에 있는 파일들은 인증을 무시하도록 설정
         web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 
