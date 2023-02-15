0928d4529fddf6e64de15ab974b870de9d8d0383
B-joon <dosel45@gmail.com>
2023-02-15 16:50:11 +0900

    Entity 관련 설명 추가

--- src/main/java/com/example/shopping_mall/entity/BaseTimeEntity.java
+++ src/main/java/com/example/shopping_mall/entity/BaseTimeEntity.java
@@ -12,6 +12,8 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @EntityListeners(value = {AuditingEntityListener.class})
+// 공통 매핑 정보가 필요할 때 사용하는 어노테이션으로 부모 클래스를 상속 받는
+// 자식 클래스에 매핑 정보만 제공한다.
 @MappedSuperclass
 @Getter @Setter
 public class BaseTimeEntity {
